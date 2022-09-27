cd791187efb74a99e9ca2c584abc0e1dbf1e5765
Filip <lazarevicfilip22@gmail.com>
2022-09-27 12:02:10 +0200

    added recursive func

--- UserManagment/Controllers/GroupsController.cs
+++ UserManagment/Controllers/GroupsController.cs
@@ -5,11 +5,15 @@ using System.Linq;
 using UserManagment.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Collections;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using System.Collections.Generic;
 
 namespace UserManagment.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(Roles = "administrator")]
     public class GroupsController : ControllerBase
     {
         private readonly UserManagmentDataContext context;
@@ -30,6 +34,10 @@ namespace UserManagment.Controllers
         [HttpGet]
         public async Task<ActionResult<GroupResult>> getGroups()
         {
+            //if(!(this.User.Claims.First(x => x.Type == ClaimTypes.Role).Value == "administrator"))
+            //{
+            //    return Forbid();
+            //}
             return Ok(await context.Groups.Select(x => new GroupResult(x.Id,x.Name)).ToArrayAsync());
         }
         [HttpGet]
@@ -44,6 +52,44 @@ namespace UserManagment.Controllers
             if (group == null)
                 return NotFound();
             return Ok(group.ChildGroups!.Select(x => new GroupResult(x.Id,x.Name)));
+        }
+        [HttpGet]
+        [Route("{id}/users")]
+
+        public async Task<ActionResult<UserResult>> getUserMembersOfGroup(int id, [FromQuery] bool recursive = false)
+        {
+            if (!await context.Groups.AnyAsync(g => g.Id == id)) return NotFound();
+
+            var users = await getMembers(id, recursive);
+
+            return Ok(users.Select(u => new UserResult(u.Id, u.NameIdentifier, u.Email, u.FirstName, u.LastName)));
+        }
+        public async Task<IEnumerable<User>> getMembers(int id, bool recursive)
+        {
+            var group = await context.Groups.Include(x => x.Users).FirstOrDefaultAsync(x => x.Id == id);
+
+
+            var users = new List<User>();
+            if (group.Users!.Any())
+            {
+                users.AddRange(group.Users!.ToList());
+            }
+            if (recursive)
+            {
+               
+                await context.Entry(group).Collection(x => x.ChildGroups).LoadAsync();
+                if (group.ChildGroups!.Any())
+                {
+                    foreach (var child in group.ChildGroups!)
+                    {
+                        users.AddRange(await getMembers(child.Id, recursive));
+                    }
+                    return users;
+                }
+                
+            }
+
+            return users;
         }
     }
 }
